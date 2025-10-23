6954aca7b092cf9c7277a8a68e53eadef1c8a1f0
101Mischief <brooke.metoxensmith@mymail.nwtc.edu>
2025-10-23 13:10:21 -0500

    Added a class. Testing Commit. Pull successful. Merge with intelliJ successful.

---
+++ src/Room1.java
@@ -0,0 +1,17 @@
+public class Room1{
+
+    private boolean userSelection = false;
+
+    super();
+    //constructor
+
+    public Room1(String userSelection){
+        this.userSelection = true;
+       //do what is needed
+
+    }
+
+
+    //close the userSelection and send back for another
+    userSelection(false);
+}
